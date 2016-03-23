4f048149ae9fca96486ab951c1ec15217bfaf0c4
Muh Rusdi <muhrusdi@muhs-macbook-pro.local>
2016-03-23 13:20:16 +0800

    Initial Commit

---
+++ Al Balagh C/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Al Balagh C
+//
+//  Created by Muh Rusdi on 3/23/16.
+//  Copyright © 2016 MRCode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Al Balagh C/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Al Balagh C
+//
+//  Created by Muh Rusdi on 3/23/16.
+//  Copyright © 2016 MRCode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
